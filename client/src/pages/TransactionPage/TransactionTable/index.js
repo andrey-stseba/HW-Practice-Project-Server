f08f5cd03c688153bf9364ade70169791e70cad3
andrey-stseba <scebaandru@gmail.com>
2021-11-25 19:03:33 +0200

    Refactor TransactionTable

--- client/src/pages/TransactionPage/TransactionTable/index.js
+++ client/src/pages/TransactionPage/TransactionTable/index.js
@@ -9,32 +9,31 @@ export const TransactionTable = props => {
   } = props;
 
   return (
-    <table className={s.transactionTable}>
-      <caption className={s.transactionCaption}>
-        <h4 className={s.headerName}>
-          Hi,
-          <span className={s.info}>{firstName}</span>
-          <span className={s.info}>{lastName}</span>
-        </h4>
-        Transaction Table
-      </caption>
-      <thead>
-        <tr>
-          <th className={s.label}>Date</th>
-          <th className={s.label}>Operation type </th>
-          <th className={s.label}>Amount</th>
-        </tr>
-      </thead>
-      <tbody>
-        {transactions.map(({ id, date, operationType, amount }) => (
-          <tr key={id}>
-            <td className={s.info}>{date}</td>
-            <td className={s.info}>{operationType}</td>
-            <td className={s.info}>{amount}</td>
+    <>
+      <h4 className={s.headerName}>
+        <span className={s.info}> Hi, {firstName}</span>
+        <span className={s.info}>{lastName}</span>
+      </h4>
+      <table className={s.transactionTable}>
+        <caption className={s.transactionCaption}>Transaction Table</caption>
+        <thead>
+          <tr>
+            <th className={s.label}>Date</th>
+            <th className={s.label}>Operation type </th>
+            <th className={s.label}>Amount</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {transactions.map(({ id, date, operationType, amount }) => (
+            <tr key={id}>
+              <td className={s.info}>{date}</td>
+              <td className={s.info}>{operationType}</td>
+              <td className={s.info}>{amount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 };
 
--- client/src/pages/TransactionPage/index.js
+++ client/src/pages/TransactionPage/index.js
@@ -9,6 +9,8 @@ const transactions = [
   { id: 1, date: '2021-08-31', operationType: 'INCOME', amount: 10 },
   { id: 2, date: '2021-09-01', operationType: 'INCOME', amount: 20 },
   { id: 3, date: '2021-09-04', operationType: 'INCOME', amount: 40 },
+  { id: 4, date: '2021-09-04', operationType: 'INCOME', amount: 60 },
+  { id: 5, date: '2021-09-04', operationType: 'INCOME', amount: 80 },
 ];
 
 function TransactionPage (props) {
